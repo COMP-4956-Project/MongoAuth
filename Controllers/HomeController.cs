cb5d3e6f648e559ddd275e88154ee0c7e7c51841
Reynard <loreynard@gmail.com>
2023-11-13 20:07:30 -0800

    Added GridFS package & Text upload to DB

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,26 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using mongoAuth.Models;
+using MongoDB.Driver;
+using MongoDB.Driver.GridFS;
+using Microsoft.AspNetCore.Http;
+using System.Text;
+using MongoDB.Bson;
 
 namespace mongoAuth.Controllers;
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
 
-    public HomeController(ILogger<HomeController> logger)
+    private IMongoDatabase db;
+    private IGridFSBucket gridFS;
+
+    public HomeController()
     {
-        _logger = logger;
+        string connectionString = "mongodb+srv://admin:Wk2hn25gmfv1JpFh@comp4956.9hedzlx.mongodb.net/";
+        MongoClient client = new MongoClient(connectionString);
+        db = client.GetDatabase("CodeCraft");
+        gridFS = new GridFSBucket(db);
     }
 
     public IActionResult Index()
@@ -21,6 +31,24 @@ public class HomeController : Controller
     public IActionResult Privacy()
     {
         return View();
+    }
+
+        [HttpPost]
+    public ActionResult UploadText()
+    {
+        string textContent = "This is the json content";
+
+        var fileName = "jas0n.txt";
+
+        byte[] contentBytes = System.Text.Encoding.UTF8.GetBytes(textContent);
+
+        var stream = new MemoryStream(contentBytes);
+
+        ObjectId fileId = gridFS.UploadFromStream(fileName, stream);
+        // Console.WriteLine("File created and data written to GridFS with ID: " + fileId);
+
+
+        return RedirectToAction("Index");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
